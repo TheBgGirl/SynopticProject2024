db44051044f8b63312add007a485c885af6235b5
Jamie Wales <jamie.wales.developer@gmail.com>
2024-06-03 18:08:34 +0100

    redirect to locator if we haven't got res/user

--- app/src/main/java/com/example/farmsimulator/utils/Dialog.kt
+++ app/src/main/java/com/example/farmsimulator/utils/Dialog.kt
@@ -1,7 +1,6 @@
 package com.example.farmsimulator.utils
 
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
