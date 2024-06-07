f82026d264b2f37442a08fd04b1e77657430eacf
Jacob Edwards <jacob.john1@talktalk.net>
2024-06-07 17:50:56 +0100

    see prev

---
+++ app/src/main/java/com/example/farmsimulator/stores/SettingsStore.kt
@@ -0,0 +1,28 @@
+package com.example.farmsimulator.stores
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore(name = "settings")
+
+class SettingsRepository(context: Context) {
+    private val dataStore = context.dataStore
+
+    companion object {
+        val LOW_DATA_MODE = booleanPreferencesKey("low_data_mode")
+    }
+
+    val lowDataModeFlow: Flow<Boolean> = dataStore.data.map { preferences ->
+        preferences[LOW_DATA_MODE] ?: false
+    }
+
+    suspend fun setLowDataMode(lowDataMode: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[LOW_DATA_MODE] = lowDataMode
+        }
+    }
+}
