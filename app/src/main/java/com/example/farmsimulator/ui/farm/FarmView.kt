2da0ea66e745d899493a7bc1bc71c6ac873c229b
Jacob Edwards <jacob.john1@talktalk.net>
2024-06-08 19:00:56 +0100

    background

--- app/src/main/java/com/example/farmsimulator/ui/farm/FarmView.kt
+++ app/src/main/java/com/example/farmsimulator/ui/farm/FarmView.kt
@@ -1,8 +1,10 @@
 package com.example.farmsimulator.ui.farm
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.farmsimulator.opengl.OpenGLComposeView
 import com.google.android.gms.maps.model.LatLng
@@ -24,6 +27,7 @@ fun FarmView(latLng: LatLng, width: Int, height: Int, crops: List<CropInfo>) {
         modifier = Modifier
             .fillMaxWidth()
             .height(300.dp)
+            .background(Color.White)
     ) {
         OpenGLComposeView(modifier = Modifier.height(250.dp), width = width, height = height, crops = crops, onClick = {
             selected = it
